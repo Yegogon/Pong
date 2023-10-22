5562bdefd8724aa52bd5842dbdf065a58a86891c
Yegogon <141447634+Yegogon@users.noreply.github.com>
2023-10-22 16:53:27 +0200

    Big Changes!
    - Added new tutorial scene
    - Flipped the Opponent Score Indicator in Singleplayer, because it only has to be shown from one side

--- Assets/Scripts/Menu-scripts/Buttons/MainMenuScripted.cs
+++ Assets/Scripts/Menu-scripts/Buttons/MainMenuScripted.cs
@@ -12,6 +12,8 @@ public class MainMenuScripted : MonoBehaviour
     [Tooltip("Write down the asset name of this scene")]
     [SerializeField] private string Singleplayer = "";
     [Tooltip("Write down the asset name of this scene")]
+    [SerializeField] private string Tutorial = "";
+    [Tooltip("Write down the asset name of this scene")]
     [SerializeField] private string Settings = "";
     [Tooltip("Write down the asset name of this scene")]
     [SerializeField] private string FAQ = "";
@@ -24,6 +26,7 @@ public class MainMenuScripted : MonoBehaviour
     public void LoadTwoPlayerGame() => SceneManager.LoadScene(TwoPlayerGame);
 
     public void LoadSingleplayer() => SceneManager.LoadScene(Singleplayer);
+    public void LoadTutorial() => SceneManager.LoadScene(Tutorial);
 
     public void LoadSettings() => SceneManager.LoadScene(Settings);
 
